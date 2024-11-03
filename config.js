384a883102fc16ffafb759cb2886376072a55452
Ace <121753552+acejobbie@users.noreply.github.com>
2024-11-03 22:16:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 
 
 require("./all/module")
-global.owner = "254742491666" 
-global.namaCreator = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
+global.owner = "254726856795" 
+global.namaCreator = "ན🏴‍☠️⃝🅐ϲԑ 𝚔𝖎𝐫қ ❬𝕯❭" 
 global.autoJoin = false 
 global.antilink = false 
 global.versisc = '6.0' 
@@ -12,10 +12,10 @@ global.isLink = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
 global.sessionName = 'session'
 global.thumb = fs.readFileSync("./thumb.png") 
 global.audionya = fs.readFileSync("./all/sound.mp3") 
-global.packname = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
-global.author = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
+global.packname = "ན🏴‍☠️⃝🅐ϲԑ 𝚔𝖎𝐫қ ❬𝕯❭" 
+global.author = "ɪ αɱ คϲє ❬𝟏❭" 
 global.jumlah = "5" 
-global.namabot = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
+global.namabot = "🏴‍☠️⃝🅐ϲԑ BOT" 
 global.domain = 'https://kingsam.sellerpanelerol.me' // your domian
 global.apikey = 'ptla_VRxkhvalZQ3EhHyAax9QYxgT5cbsx2MrO69gBpVZmmd' // Isi Apikey Plta Lu
 global.capikey = 'ptlc_sQ3d4GxCiztyV5DbhUJSWtvnSCyKzeMXoWIr2lVyqal' // Isi Apikey Pltc Lu
